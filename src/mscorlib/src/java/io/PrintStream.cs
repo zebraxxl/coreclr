9df629370b4783d831cb581b560cdf1cfcefdec3
zebraxxl <zebraxxl@mail.ru>
2015-02-24 01:12:38 +0300

    java and CIL2Java namespaces added

---
+++ src/mscorlib/src/java/io/PrintStream.cs
@@ -0,0 +1,28 @@
+﻿using CIL2Java.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace java.io
+{
+    [FromJava()]
+    public class PrintStream
+    {
+        [FromJava]
+        public void print(char ch) { }
+
+        [FromJava()]
+        public void print(int i) { }
+
+        [FromJava()]
+        public void print(string s) { }
+
+        [FromJava]
+        public void println(char ch) { }
+
+        [FromJava()]
+        public void println(int i) { }
+
+        [FromJava()]
+        public void println(string s) { }
+    }
+}
---
+++ src/mscorlib/src/java/lang/System.cs
@@ -0,0 +1,22 @@
+﻿using CIL2Java.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace java.lang
+{
+    [FromJava()]
+    public sealed class System
+    {
+        /** Don't let anyone instantiate this class */
+        private System() { }
+
+        [FromJava()]
+        public static io.PrintStream @out;
+
+        [FromJava]
+        public static void arraycopy(object src, int srcPos, object dest, int destPos, int length) { }
+
+
+        [FromJava] public static string getProperty(string key) { return null; }
+    }
+}
